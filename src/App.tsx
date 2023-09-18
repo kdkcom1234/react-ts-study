ed9385527f58dbd636ccfac17e6c1487c3e481dc
Daekeun Ko <kdkcom1234@gmail.com>
2023-09-18 17:12:56 +0900

    children 속성 받기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import Counter from "./components/Counter";
+import Layout from "./components/Layout";
 import Todo from "./components/Todo";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
   return (
@@ -7,6 +9,12 @@ const App = () => {
       <Counter />
       <hr />
       <Todo />
+      <hr />
+      <Layout title="Home Page">
+        {/* children 속성을 안쪽 태그에 */}
+        <WelcomeMessage name="Alice" />
+        <p>Welcome to our website!</p>
+      </Layout>
     </>
   );
 };
