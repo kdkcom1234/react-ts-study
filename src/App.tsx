c4530e024c2269c40b54170da36a20a5329597b6
Daekeun Ko <kdkcom1234@gmail.com>
2023-09-20 16:18:40 +0900

    글로벌 스타일

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import Todo from "./components/todo/Todo";
+import ResetStyle from "./styles/reset";
 
 const App = () => {
   return (
     <>
+      {/* 글로벌 스타일을 가장 첫부분에 넣어야함 */}
+      <ResetStyle />
       <Todo />
     </>
   );
