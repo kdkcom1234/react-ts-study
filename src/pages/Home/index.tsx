08b3999629832b4203cae669195a1c3a11bfbc6e
Daekeun Ko <kdkcom1234@gmail.com>
2023-12-04 13:09:54 +0900

    수정 테스트

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <div>
-      <h1>Home Page, Rebuild</h1>
+      <h1>
+        Home Page, Rebuild - public, invalidation
+      </h1>
       <h2>Home Page, Rebuild</h2>
     </div>
   );
